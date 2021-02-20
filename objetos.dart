16e6bca6b2bb4efb28ef568ed409bec08a8f9115
paulohenrique085 <paulohnq85@outlook.com>
2021-02-19 23:51:22 -0300

    estudo

--- objetos.dart
+++ objetos.dart
@@ -1,4 +1,22 @@
-import 'dart:io';
-
 //criando objetos em dart aula 1.2
-void main() {}
+
+class Pessoa {
+  int idade;
+  String nome;
+//construtor da class
+  Pessoa(this.idade, this.nome);
+//método
+  verificacao() {
+    if (idade >= 50) {
+      print("Aposentadoria aprovada!");
+    } else {
+      print("Aposentadoria reprovada!");
+    }
+  }
+}
+
+void main() {
+  Pessoa paulo = Pessoa(50, "paulo");
+
+  paulo.verificacao();
+}
